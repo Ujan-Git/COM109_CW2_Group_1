57f0fb5bee15a0a4489dfa7439d2c39e01c594b4
Kabita0226 <Saud-K@ulster.ac.uk>
2025-08-11 15:28:58 +0100

    Kabita:added header

--- Contact/contactquery.js
+++ Contact/contactquery.js
@@ -1,12 +1,8 @@
 $(document).ready(function () {
-
   $('#contactForm').submit(function (e) {
     e.preventDefault();
-    const name = $('input[name="firstName"]').val();
-    alert(`Thanks ${name}, your message has been sent!`);
+    const firstName = $('input[name="firstName"]').val();
+    alert(`Thanks, ${firstName}! Your message has been sent.`);
     $(this)[0].reset();
-  });
- $('#nav-contact').click(function () {
-    $('#contact-section').removeClass('hidden');
   });
 });
